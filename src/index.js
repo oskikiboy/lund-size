789a298e6f9413633146a9153fc83bb8157430c0
Oscar <oscardavies1@gmail.com>
2018-01-12 11:39:35 +1000

    SUPER SPEED

--- src/index.js
+++ src/index.js
@@ -7,32 +7,100 @@
  */
 
 const figlet = require("util").promisify(require("figlet"));
+const cluster = require("cluster");
+const colors = require("colors");
+colors.enabled
+var x = 0;
+
+const spamme = () => {
+    setTimeout(function () {
+        let infinite = 2;
+        for (null; x < infinite; x++) {
+            infinite++;
+
+            if (process.argv.includes("--ascii")) {
+                asciify()
+            } else {
+                if (process.argv.includes("--nonum")) {
+                    console.log(`${determine()}`)
+                } else {
+                    console.log(`No. ${x}\n\t${determine()}`)
+                }
+            }
+
+        }
+
+    }, 5000);
+}
+
+const countdown = () => {
+    setTimeout(function () {
+        console.log("3".red)
+        setTimeout(function () {
+            console.log("2".yellow)
+            setTimeout(function () {
+                console.log("1".green)
+                setTimeout(function () {
+                    console.log("GO".bold.white)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}
 
 const determine = () => {
     if (process.argv.includes("--china")) {
         return `8=D`;
     } else {
-        return `8${"=".repeat(Math.floor(Math.random() * 12) + 1)}D`
+        return `8${"=".repeat(Math.floor(Math.random() * 175) + 1)}D`
     }
 }
 
 const asciify = () => {
-figlet(determine()).then(data => console.log(`Your lund length is: \n${data}`)).catch(err => {
-            console.log('Something went wrong...');
-            console.dir(err);
-            return;
+    figlet(determine()).then(data => console.log(`Your lund length is: \n${data}`)).catch(err => {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
     });
 }
+const main = () => {
+    if (process.argv.includes("--spam")) {
 
-try {
+        console.log("Running in " + "spam".red + " mode")
 
-    if (process.argv.includes("--ascii")) {
-        asciify()
+        countdown()
+
+        spamme()
+
+    } else if (process.argv.includes("--superspam")) { 
+
+        console.log("Running in " + "super".bgRed + " " + "spam".red + " mode")
+
+        countdown()
+
+        spamme()
+
+        spamme()
+
+        spamme()
+
+        spamme()
     } else {
-        console.log(`Your lund length is: \n\t${determine()}`)
-    }
 
-} catch (err) {
+        if (process.argv.includes("--ascii")) {
+            asciify()
+        } else {
+            console.log(`Your lund length is: \n\t${determine()}`)
+        }
+    }
+}
+
+const main_promised = require("util").promisify(main)
+
+
+main_promised().then(oWo => {
+    console.log("if you see this text, something is wrong", oWo)
+}).catch(e => {
     console.error(`Error determening penis size, ${err}`)
     console.debug(`Error Stacktrace, ${err.stack}`)
-}
+})
